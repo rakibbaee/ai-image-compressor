a421f06d66b3865fb634fd504111f3012cc01d52
Rakib Bae <159868325+rakibbaee@users.noreply.github.com>
2026-05-09 10:12:08 +0600

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://ai-image-compressor-beige.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
