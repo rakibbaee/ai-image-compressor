247aeee88d8dc1f007ed54364f49684bfc2f90d3
Rakib Bae <159868325+rakibbaee@users.noreply.github.com>
2026-05-09 10:12:40 +0600

    Add robots.js for handling web crawler rules

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://ai-image-compressor-beige.vercel.app/sitemap.xml',
+  }
+}
